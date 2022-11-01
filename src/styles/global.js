4ac5f4dd146c023eb63af183340db53a29eb18da
MatheusTavares94 <mmatheus_ttavares@hotmail.com>
2022-11-01 19:29:41 -0300

    feature(Update): Font update

--- src/styles/global.js
+++ src/styles/global.js
@@ -5,8 +5,7 @@ export const GlobalStyle = createGlobalStyle`
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande',
-    'Lucida Sans', Arial, sans-serif;
+  font-family: 'Poppins', sans-serif;
 }
 
 body {
